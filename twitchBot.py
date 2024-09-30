d65ca039770035b435f2f447cabba61705a00ad9
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2024-09-30 06:08:42 +0200

    bot listen in a dedicated thread

--- twitchBot.py
+++ twitchBot.py
@@ -1,6 +1,7 @@
 from decouple import config
 import socket
 import re
+import threading
 class TwitchBot():
     
     serveur = "irc.chat.twitch.tv"
@@ -10,24 +11,42 @@ class TwitchBot():
     channel = "#rs_appez"
 
     def __init__(self):
-        self.connect()
+
+        self.__start()
+
+
+    def __start(self):
+        self.__connect()
+
+        threading.Thread(target=self.__listen,name="Bot irc").start()
+
 
         while True:
             response = self.irc.recv(2048).decode("utf-8")
             if response.startswith("PING"):
                 self.irc.send("PONG\n".encode("utf-8"))
             else:
-                self.parse_message(response)
-               
-    def connect(self):
+                self.__parse_message(response)
+
+    def __connect(self):
         self.irc = socket.socket()
         self.irc.connect((self.serveur, self.port))
         self.irc.send(f"PASS {self.token}\n".encode("utf-8"))
         self.irc.send(f"NICK {self.nickname}\n".encode("utf-8"))
         self.irc.send(f"JOIN {self.channel}\n".encode("utf-8"))
 
-    def parse_message(self, message):
+    def __listen(self):
+        while True:
+            response = self.irc.recv(2048).decode("utf-8")
+            if response.startswith("PING"):
+                self.irc.send("PONG\n".encode("utf-8"))
+            else:
+                self.__parse_message(response)
+
+    def __parse_message(self, message):
         r = re.search(':(.*)\!.*@.*\.tmi\.twitch\.tv PRIVMSG #(.*) :(.*)', message)
         if r:
             username, channel, message = r.groups()
             print(username + ": " + message)
+
+    
