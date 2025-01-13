95dd6b0fdf5e170475c50d990f5ad6345dc9483e
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-01-13 13:58:08 +0100

    add buy method

--- pokeCommu.py
+++ pokeCommu.py
@@ -4,13 +4,19 @@ import requests
 
 class PokeCommu:
 
-    url = "https://poketwitch.bframework.de/api/game/ext/trainer/"
+    url = "https://poketwitch.bframework.de/api/game/ext/"
 
-    url_poke = url + "pokemon/"
-    url_inventory = url + "inventory/v2/"
-    url_trade = url + "wonder-trade/"
+    url_trainer = url + "trainer/"
+    url_shop = url + "shop/"
+
+    url_poke = url_trainer + "pokemon/"
+    url_inventory = url_trainer + "inventory/v2/"
+    url_trade = url_trainer + "wonder-trade/"
+
+    url_purchase = url_shop + "purchase/"
 
     token = config("PCG_TOKEN")
+    header = {"Authorization": token}
 
     def __init__(self):
         self.pokemons = []
@@ -26,7 +32,7 @@ class PokeCommu:
         return True
 
     def get_pokemon(self):
-        response = requests.get(self.url_poke, headers={"Authorization": self.token})
+        response = requests.get(self.url_poke, headers=self.header)
         if response.status_code == 200:
             for pokemon in response.json()["allPokemon"]:
                 if not pokemon["isShiny"]:
@@ -38,9 +44,7 @@ class PokeCommu:
             return False
 
     def get_inventory(self):
-        response = requests.get(
-            self.url_inventory, headers={"Authorization": self.token}
-        )
+        response = requests.get(self.url_inventory, headers=self.header)
         if response.status_code == 200:
             for ball in response.json()["allItems"]:
                 if ball["type"] == 2:
@@ -52,11 +56,21 @@ class PokeCommu:
     def trade_pokemon(self, pokemon_id):
 
         response = requests.post(
-            self.url_trade + str(pokemon_id) + "/",
-            headers={"Authorization": self.token},
+            self.url_trade + str(pokemon_id) + "/", headers=self.header
         )
         print(response.content)
         if response.status_code == 200:
             return True
         else:
             return False
+
+    def buy_item(self, item, amount=1):
+
+        data = {"amount": amount, "item_name": item}
+
+        response = requests.post(self.url_purchase, headers=self.header, data=data)
+
+        print(response.json())
+        if response.status_code == 200:
+            return True
+        return False
