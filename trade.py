50dea606f1bde2f577974deaf3331fbb576d5bfe
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-02-28 05:37:31 +0100

    use match/case for better error handling

--- trade.py
+++ trade.py
@@ -3,35 +3,41 @@ import sys
 from pokeBusiness import PokeBusiness
 
 if __name__ == "__main__":
+    args = sys.argv
+    pb = PokeBusiness()
+
+    # type args
     type = None
     level = None
     speed = None
     hp = None
     defSpe = None
 
+    # sort args
     sort = None
     base = False
 
-    args = sys.argv
+    print(args)
+    for arg in args[1:]:
+        match arg:
+            case _ if arg.startswith("type="):
+                type = arg[5:]
+            case _ if arg.startswith("level="):
+                level = arg[6:]
+            case _ if arg.startswith("speed="):
+                speed = arg[6:]
+            case _ if arg.startswith("defSpe="):
+                defSpe = arg[7:]
+            case _ if arg.startswith("hp="):
+                hp = arg[3:]
+            case _ if arg.startswith("sort="):
+                sort = arg[5:]
+            case "base":
+                base = True
+            case _:
+                print(f"Invalid argument: {arg}")
+                sys.exit(1)
 
-    for arg in args:
-        if arg.startswith("type="):
-            type = arg[5:]
-        if arg.startswith("level="):
-            level = arg[6:]
-        if arg.startswith("speed="):
-            speed = arg[6:]
-        if arg.startswith("defSpe="):
-            defSpe = arg[7:]
-        if arg.startswith("hp="):
-            hp = arg[3:]
-
-        if arg.startswith("sort="):
-            sort = arg[5:]
-        if arg.startswith("base"):
-            base = True
-
-    pb = PokeBusiness()
     pb.auto_trade(
         type=type, level=level, speed=speed, sort=sort, hp=hp, defSpe=defSpe, base=base
     )
