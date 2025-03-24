0f7f36923fc86df4f6a67a1f7988a7fe1e72b250
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-03-24 01:15:03 +0100

    add BallBusiness

---
+++ ballBusiness.py
@@ -0,0 +1,204 @@
+import asyncio
+import random
+
+
+class BallBusiness:
+    def __init__(self, inventory):
+        self.inventory = inventory
+
+    def find_best_ball(self, pokemon):
+        pokemons = self.pokeCommu.pokemons
+        pokemons_shiny = self.pokeCommu.pokemons_shiny
+
+        # Check if the pokemon is already caught
+        if any(poke["name"] == pokemon.en_name for poke in pokemons):
+            if not any(poke["name"] == pokemon.en_name for poke in pokemons_shiny):
+                if best_ball := self.__check_cherish_ball():
+                    return best_ball
+            if best_ball := self.__check_drop_ball():
+                return best_ball
+            if best_ball := self.__check_duplicate_ball():
+                return best_ball
+
+        checks = [
+            self.__check_time_ball,
+            lambda: self.__check_weight_ball(pokemon.weight),
+            lambda: self.__check_type_ball(pokemon.en_types),
+            lambda: self.__check_stats_ball(pokemon.stats),
+            self.__check_ultra_ball,
+            self.__check_drop_ball,
+            self.__check_low_ball,
+        ]
+
+        for check in checks:
+            if best_ball := check():
+                return best_ball
+
+        return None
+
+    def check_ball_in_inventary(self, ball):
+        if [b for b in self.inventory if b["sprite_name"] == ball]:
+            ball = [b for b in self.inventory if b["sprite_name"] == ball][0]
+            if ball["amount"] > 0:
+                ball["amount"] -= 1
+                return True
+
+        return False
+
+    def __check_cherish_ball(self):
+        # 80%
+        if self.check_ball_in_inventary("ultra_cherish_ball"):
+            best_ball = "ultracherishball"
+            self.__wait()
+            return best_ball
+        # 55%
+        if self.check_ball_in_inventary("great_cherish_ball"):
+            best_ball = "greatcherishball"
+            self.__wait()
+            return best_ball
+        # 30%
+        if self.check_ball_in_inventary("cherish_ball"):
+            best_ball = "cherishball"
+            self.__wait()
+            return best_ball
+        return None
+
+    def __check_drop_ball(self):
+        # 55%
+        if self.check_ball_in_inventary("stone_ball"):
+            best_ball = "stoneball"
+            self.__wait()
+            return best_ball
+        return None
+
+    def __check_duplicate_ball(self):
+        if self.check_ball_in_inventary("repeat_ball"):
+            best_ball = "repeatball"
+            self.__wait()
+            return best_ball
+        return None
+
+    def __check_time_ball(self):
+        if self.check_ball_in_inventary("quick_ball"):
+            best_ball = "quickball"
+            self.__wait(random.randint(1, 5))
+            return best_ball
+
+        if self.check_ball_in_inventary("timer_ball"):
+            best_ball = "timerball"
+            self.__wait(random.randint(1, 5))
+            self.__wait(70)
+            return best_ball
+        return None
+
+    def __check_weight_ball(self, weight):
+        if weight > 204.8:
+            if self.check_ball_in_inventary("heavy_ball"):
+                best_ball = "heavyball"
+                self.__wait()
+                return best_ball
+
+        if weight <= 9.9:
+            if self.check_ball_in_inventary("feather_ball"):
+                best_ball = "featherball"
+                self.__wait()
+                return best_ball
+        return None
+
+    def __check_type_ball(self, types):
+        type_ball_mapping = {
+            "Ice": "frozen_ball",
+            "Normal": "basic_ball",
+            "Dark": "night_ball",
+            "Ghost": "phantom_ball",
+            "Flying": "mach_ball",
+            "Fighting": "mach_ball",
+            "Poison": "cipher_ball",
+            "Psychic": "cipher_ball",
+            "Steel": "magnet_ball",
+            "Electric": "magnet_ball",
+            "Fairy": "fantasy_ball",
+            "Dragon": "fantasy_ball",
+            "Fire": "sun_ball",
+            "Grass": "sun_ball",
+            "Water": "net_ball",
+            "Bug": "net_ball",
+            "Rock": "geo_ball",
+            "Ground": "geo_ball",
+        }
+
+        for t in types:
+            if ball := type_ball_mapping.get(t):
+                if self.check_ball_in_inventary(ball):
+                    best_ball = ball.replace("_ball", "ball")
+                    self.__wait()
+                    return best_ball
+
+        return None
+
+    def __check_stats_ball(self, stats):
+        if stats["vit"] > 100:
+            if self.check_ball_in_inventary("fast_ball"):
+                best_ball = "fastball"
+                self.__wait()
+                return best_ball
+
+        if stats["hp"] > 100:
+            if self.check_ball_in_inventary("heal_ball"):
+                best_ball = "healball"
+                self.__wait()
+                return best_ball
+
+        return None
+
+    def __check_ultra_ball(self):
+        if self.check_ball_in_inventary("ultra_ball"):
+            best_ball = "ultraball"
+            self.__wait()
+            return best_ball
+        return None
+
+    def __check_low_ball(self):
+        # 55%
+        if self.check_ball_in_inventary("great_ball"):
+            best_ball = "greatball"
+            self.__wait()
+            return best_ball
+
+        if self.check_ball_in_inventary("luxury_ball"):
+            best_ball = "luxuryball"
+            self.__wait()
+            return best_ball
+
+        # 40%
+        if self.check_ball_in_inventary("level_ball"):
+            best_ball = "levelball"
+            self.__wait()
+            return best_ball
+
+        # 30%
+        if self.check_ball_in_inventary("clone_ball"):
+            best_ball = "cloneball"
+            self.__wait()
+            return best_ball
+
+        if self.check_ball_in_inventary("poke_ball"):
+            best_ball = " "
+            self.__wait()
+            return best_ball
+
+        if self.check_ball_in_inventary("premier_ball"):
+            best_ball = " "
+            self.__wait()
+            return best_ball
+        return None
+
+    def __wait(self, time: int = 0):
+        if not time:
+            time = random.randint(5, 80)
+
+        asyncio.run(self.__wait_coroutine(time))
+
+    async def __wait_coroutine(self, time: int):
+        await asyncio.sleep(time)
+        return True
--- pokeBusiness.py
+++ pokeBusiness.py
@@ -1,19 +1,19 @@
 from pokeCommu import PokeCommu
 from pokemonData import PokemonData
-import asyncio
-import random
+from ballBusiness import BallBusiness
 
 
 class PokeBusiness:
     def __init__(self):
         self.pokemon_data = PokemonData()
         self.pokeCommu = PokeCommu()
+        self.ballBusiness = BallBusiness(self.pokeCommu.inventory)
 
     def catch_pokemon(self, pokemon):
         poke_data = self.pokemon_data.get_pokemon(pokemon, "en")
 
         if poke_data:
-            ball = self.find_best_ball(poke_data)
+            ball = self.ballBusiness.find_best_ball(poke_data)
             if ball:
                 return ball
             else:
@@ -51,26 +51,6 @@ class PokeBusiness:
         else:
             print("No pokemon to trade")
 
-    def __check_ball_in_inventary(self, ball):
-        balls = self.pokeCommu.inventory
-
-        if [b for b in balls if b["sprite_name"] == ball]:
-            ball = [b for b in balls if b["sprite_name"] == ball][0]
-            if ball["amount"] > 0:
-                ball["amount"] -= 1
-                return True
-
-        return False
-
-    def __wait(self, time: int = 0):
-        if not time:
-            time = random.randint(5, 80)
-
-        asyncio.run(self.__wait_coroutine(time))
-
-    async def __wait_coroutine(self, time: int):
-        await asyncio.sleep(time)
-        return True
 
     def __get_first_duplicated_pokemon(
         self,
@@ -217,192 +197,3 @@ class PokeBusiness:
             for pokemon in self.pokeCommu.pokemons
             if pokemon.get("pokedexId") == id
         ]
-
-    def find_best_ball(self, pokemon):
-        best_ball = None
-
-        pokemons = self.pokeCommu.pokemons
-        pokemons_shiny = self.pokeCommu.pokemons_shiny
-
-        types = [type for type in pokemon.en_types]
-        stats = pokemon.stats
-        # Check if the pokemon is already caught
-        if [poke for poke in pokemons if poke["name"] == pokemon.en_name]:
-            # Check if the pokemon is shiny
-            if not [poke for poke in pokemons_shiny if poke["name"] == pokemon.en_name]:
-                # 80%
-                if self.__check_ball_in_inventary("ultra_cherish_ball"):
-                    best_ball = "ultracherishball"
-                    self.__wait()
-                    return best_ball
-                # 55%
-                if self.__check_ball_in_inventary("great_cherish_ball"):
-                    best_ball = "greatcherishball"
-                    self.__wait()
-                    return best_ball
-                # 30%
-                if self.__check_ball_in_inventary("cherish_ball"):
-                    best_ball = "cherishball"
-                    self.__wait()
-                    return best_ball
-
-            if self.__check_ball_in_inventary("stone_ball"):
-                best_ball = "stoneball"
-                self.__wait()
-                return best_ball
-
-            if self.__check_ball_in_inventary("repeat_ball"):
-                best_ball = "repeatball"
-                self.__wait()
-                return best_ball
-
-        if self.__check_ball_in_inventary("quick_ball"):
-            best_ball = "quickball"
-            self.__wait(random.randint(1, 5))
-            return best_ball
-
-        if self.__check_ball_in_inventary("timer_ball"):
-            best_ball = "timerball"
-            self.__wait(random.randint(1, 5))
-            self.__wait(70)
-            return best_ball
-
-        if pokemon.weight > 204.8:
-            if self.__check_ball_in_inventary("heavy_ball"):
-                best_ball = "heavyball"
-                self.__wait()
-                return best_ball
-
-        if pokemon.weight <= 9.9:
-            if self.__check_ball_in_inventary("feather_ball"):
-                best_ball = "featherball"
-                self.__wait()
-                return best_ball
-
-        # Check type ball
-
-        # 80%
-        if "Ice" in types:
-            if self.__check_ball_in_inventary("frozen_ball"):
-                best_ball = "frozenball"
-                self.__wait()
-                return best_ball
-
-        if "Normal" in types:
-            if self.__check_ball_in_inventary("basic_ball"):
-                best_ball = "basicball"
-                self.__wait()
-                return best_ball
-
-        if "Dark" in types:
-            if self.__check_ball_in_inventary("night_ball"):
-                best_ball = "nightball"
-                self.__wait()
-                return best_ball
-
-        if "Ghost" in types:
-            if self.__check_ball_in_inventary("phantom_ball"):
-                best_ball = "phantomball"
-                self.__wait()
-                return best_ball
-
-        if any(t in types for t in ["Flying", "Fighting"]):
-            if self.__check_ball_in_inventary("mach_ball"):
-                best_ball = "machball"
-                self.__wait()
-                return best_ball
-
-        if any(t in types for t in ["Poison", "Psychic"]):
-            if self.__check_ball_in_inventary("cipher_ball"):
-                best_ball = "cipherball"
-                self.__wait()
-                return best_ball
-
-        if any(t in types for t in ["Steel", "Electric"]):
-            if self.__check_ball_in_inventary("magnet_ball"):
-                best_ball = "magnetball"
-                self.__wait()
-                return best_ball
-
-        if any(t in types for t in ["Fairy", "Dragon"]):
-            if self.__check_ball_in_inventary("fantasy_ball"):
-                best_ball = "fantasyball"
-                self.__wait()
-                return best_ball
-
-        if any(t in types for t in ["Fire", "Grass"]):
-            if self.__check_ball_in_inventary("sun_ball"):
-                best_ball = "sunball"
-                self.__wait()
-                return best_ball
-
-        if any(t in types for t in ["Water", "Bug"]):
-            if self.__check_ball_in_inventary("net_ball"):
-                best_ball = "netball"
-                self.__wait()
-                return best_ball
-
-        if any(t in types for t in ["Rock", "Ground"]):
-            if self.__check_ball_in_inventary("geo_ball"):
-                best_ball = "geoball"
-                self.__wait()
-                return best_ball
-
-        # 80% with stats
-
-        if stats["vit"] > 100:
-            if self.__check_ball_in_inventary("fast_ball"):
-                best_ball = "fastball"
-                self.__wait()
-                return best_ball
-
-        if stats["hp"] > 100:
-            if self.__check_ball_in_inventary("heal_ball"):
-                best_ball = "healball"
-                self.__wait()
-                return best_ball
-
-        if self.__check_ball_in_inventary("ultra_ball"):
-            best_ball = "ultraball"
-            self.__wait()
-            return best_ball
-
-        # 55%
-        if self.__check_ball_in_inventary("stone_ball"):
-            best_ball = "stoneball"
-            self.__wait()
-            return best_ball
-
-        if self.__check_ball_in_inventary("great_ball"):
-            best_ball = "greatball"
-            self.__wait()
-            return best_ball
-
-        if self.__check_ball_in_inventary("luxury_ball"):
-            best_ball = "luxuryball"
-            self.__wait()
-            return best_ball
-
-        # 40%
-        if self.__check_ball_in_inventary("level_ball"):
-            best_ball = "levelball"
-            self.__wait()
-            return best_ball
-
-        # 30%
-        if self.__check_ball_in_inventary("clone_ball"):
-            best_ball = "cloneball"
-            self.__wait()
-            return best_ball
-
-        if self.__check_ball_in_inventary("poke_ball"):
-            best_ball = "pokeball"
-            self.__wait()
-            return best_ball
-
-        if self.__check_ball_in_inventary("premier_ball"):
-            best_ball = "premierball"
-            self.__wait()
-            return best_ball
-
-        return best_ball
