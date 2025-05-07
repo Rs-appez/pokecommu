3588d23de42a086ae19f907933cea36c98258aa2
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-05-07 23:56:50 +0200

    add help

--- trade.py
+++ trade.py
@@ -3,6 +3,18 @@ import sys
 
 from tradeBusiness import TradeBusiness
 
+help_args = [
+    ("type=<type>", "Type of the pokemon"),
+    ("level=<level>", "Level of the pokemon"),
+    ("speed=<speed>", "Speed of the pokemon"),
+    ("def=<defense>", "Defense of the pokemon"),
+    ("defSpe=<defense>", "Special Defense of the pokemon"),
+    ("hp=<hp>", "HP of the pokemon"),
+    ("bst=<bst>", "BST of the pokemon"),
+    ("sort=<sort>", "Ordering sort : gt/lt (default lt)"),
+    ("base", "Base stats only"),
+]
+
 if __name__ == "__main__":
     args = sys.argv
 
@@ -21,6 +33,11 @@ if __name__ == "__main__":
 
     for arg in args[1:]:
         match arg:
+            case "help" | "-h":
+                print("Usage: python trade.py [args]\nArgs:\n")
+                for arg, desc in help_args:
+                    print(f"  {arg:<20} {desc}")
+                sys.exit(0)
             # type args
             case _ if arg.startswith("type="):
                 poke_type = arg[5:]
@@ -39,11 +56,15 @@ if __name__ == "__main__":
 
             # sort args
             case _ if arg.startswith("sort="):
+                if arg[5:] not in ["lt", "gt"]:
+                    print(f"Invalid argument: {arg}")
+                    sys.exit(1)
                 sort = arg[5:]
             case "base":
                 base = True
             case _:
                 print(f"Invalid argument: {arg}")
+                print("Use 'help' or '-h' for more information")
                 sys.exit(1)
 
     tb = TradeBusiness(
