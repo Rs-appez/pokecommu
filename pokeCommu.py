94fbdeb868563dca22e6e81728547dfc0dba0c26
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-01-01 16:09:59 +0100

    refactor(pokecommu): add thread safety and JSON loading
    Refactored PokeCommu to use threading locks for pokemons, inventory,
    and pokedex to ensure thread safety. Replaced legacy *_tmp methods with
    JSON-based loading and saving for pokemons, inventory, and pokedex.
    Centralized data loading logic and improved method naming consistency.
    Added test code for pokedex lookups in main block.

--- pokeCommu.py
+++ pokeCommu.py
@@ -1,8 +1,10 @@
+import datetime
 import json
 from itertools import chain
 
 import requests
 from decouple import config
+from threading import Lock
 
 from models.pokemon import Pokemon
 from models.pokemonData import PokemonDataMapper
@@ -25,6 +27,8 @@ class PokeCommu:
     token = config("PCG_TOKEN")
     header = {"Authorization": token}
 
+    exeptions_pokemons = []
+
     def __init__(self):
         self.pokemons = []
         self.pokemons_locked = []
@@ -37,95 +41,121 @@ class PokeCommu:
 
         self.cash = 0
 
+        self.pekemon_lock = Lock()
+        self.inventory_lock = Lock()
+        self.pokedex_lock = Lock()
+
         self.refresh_all()
 
     def refresh_all(self):
         if not (
-            self.load_pokemon_tmp()
-            and self.load_inventory_tmp()
-            and self.load_pokedex_tmp()
+            self.load_pokemon_json()
+            and self.load_inventory_json()
+            and self.load_pokedex_json()
         ):
             print("Error while refreshing")
             return False
-        else:
-            return True
-            self.__auto_buy_ultraball()
 
         return True
 
-    def load_pokemon(self):
+    def save_pokemons_json(self):
+        with open("json/pokemons.json", "w") as file:
+            pokemons_data = {"allPokemon": []}
+            with self.pekemon_lock:
+                pokemons_data["allPokemon"].extend(self.pokemons)
+                pokemons_data["allPokemon"].extend(self.pokemons_locked)
+                pokemons_data["allPokemon"].extend(self.pokemons_shiny)
+                pokemons_data["allPokemon"].extend(self.eggs)
+
+            json.dump(pokemons_data, file, indent=4)
+
+        print(
+            f"[{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Pokemons saved to json/pokemons.json"
+        )
+
+    def load_pokemon_api(self):
         response = requests.get(self.url_poke, headers=self.header)
         if response.status_code == 200:
-            self.__load_pokemons(response.json())
+            self.load_pokemons(response.json())
             return True
         else:
             print(f"Error loading pokemon: {response.json()}")
             return False
 
-    def load_pokemon_tmp(self):
+    def load_pokemon_json(self):
         with open("json/pokemons.json", "r") as file:
             pokemons = json.load(file)
-            self.__load_pokemons(pokemons)
+            self.load_pokemons(pokemons)
             return True
 
-    def __load_pokemons(self, pokemons_data: dict):
-        for pokemon in pokemons_data["allPokemon"]:
-            if pokemon["isLoanPokemon"]:
-                continue
-            pokemon["name"] = pokemon["name"].lower()
-            if pokemon["isBuddy"]:
-                self.poke_buddy = PokemonDataMapper.get_pokemon_from_pcg(pokemon)
-            if "egg" in pokemon["name"].split("-"):
-                self.eggs.append(pokemon)
-            elif pokemon["locked"]:
-                self.pokemons_locked.append(pokemon)
-            elif pokemon["isShiny"]:
-                self.pokemons_shiny.append(pokemon)
-            else:
-                self.pokemons.append(pokemon)
+    def load_pokemons(self, pokemons_data: dict):
+        with self.pekemon_lock:
+            for pokemon in pokemons_data["allPokemon"]:
+                if pokemon["isLoanPokemon"]:
+                    continue
+                pokemon["name"] = pokemon["name"].lower()
+                if "egg" in pokemon["name"].split("-"):
+                    self.eggs.append(pokemon)
+                else:
+                    if pokemon["isBuddy"]:
+                        self.poke_buddy = PokemonDataMapper.get_pokemon_from_pcg(
+                            pokemon
+                        )
+                        print(self.poke_buddy.en_types)
+                    if pokemon["locked"]:
+                        self.pokemons_locked.append(pokemon)
+                    elif pokemon["isShiny"]:
+                        self.pokemons_shiny.append(pokemon)
+                    else:
+                        self.pokemons.append(pokemon)
 
-    def load_inventory(self):
+    def load_inventory_api(self):
         response = requests.get(self.url_inventory, headers=self.header)
+        print("response : ", response.status_code, response.text)
         if response.status_code == 200:
             json_response = response.json()
-            self.cash = json_response["cash"]
-            for ball in json_response["allItems"]:
-                if ball["type"] == 2:
-                    self.inventory.append(ball)
+            self.load_inventory(json_response)
             return True
         else:
             return False
 
-    def load_inventory_tmp(self):
+    def load_inventory_json(self):
         with open("json/inventory.json", "r") as file:
             inventory = json.load(file)
-            self.cash = inventory["cash"]
-            for ball in inventory["allItems"]:
-                if ball["type"] == 2:
-                    self.inventory.append(ball)
+            self.load_inventory(inventory)
             return True
 
-    def load_pokedex(self):
+    def load_inventory(self, inventory_data: dict):
+        with self.inventory_lock:
+            self.cash = inventory_data["cash"]
+            for ball in inventory_data["allItems"]:
+                if ball["type"] == 2:
+                    self.inventory.append(ball)
+
+    def load_pokedex_api(self):
         response = requests.get(
             self.url_pokedex,
             headers=self.header,
         )
         if response.status_code == 200:
             pokedex_json = response.json()
-            for pokemon in pokedex_json["dex"]:
-                self.pokedex[pokemon["name"]] = pokemon["c"]
+            self.load_pokedex(pokedex_json)
 
             return True
         else:
             return False
 
-    def load_pokedex_tmp(self):
+    def load_pokedex_json(self):
         with open("json/pokedex.json", "r") as file:
             pokedex_json = json.load(file)
-            for pokemon in pokedex_json["dex"]:
-                self.pokedex[pokemon["name"]] = pokemon["c"]
+            self.load_pokedex(pokedex_json)
 
             return True
+
+    def load_pokedex(self, pokedex_data: dict):
+        with self.pokedex_lock:
+            for pokemon in pokedex_data["dex"]:
+                self.pokedex[pokemon["name"]] = pokemon["c"]
 
     def get_pokemon(self, pokemon_id) -> dict | None:
         response = requests.get(
@@ -138,24 +168,25 @@ class PokeCommu:
             return None
 
     def trade_pokemon(self, pokemon_id):
-        response = requests.post(
-            self.url_trade + str(pokemon_id) + "/", headers=self.header
-        )
-        if response.status_code == 200:
-            poke_id = response.json()["pokemon"]["order"]
-            poke_name = response.json()["pokemon"]["name"]
-            poke_level = response.json()["pokemon"]["lvl"]
-            poke_avgIV = response.json()["pokemon"]["avgIV"]
+        with self.pekemon_lock:
+            response = requests.post(
+                self.url_trade + str(pokemon_id) + "/", headers=self.header
+            )
+            if response.status_code == 200:
+                poke_id = response.json()["pokemon"]["order"]
+                poke_name = response.json()["pokemon"]["name"]
+                poke_level = response.json()["pokemon"]["lvl"]
+                poke_avgIV = response.json()["pokemon"]["avgIV"]
 
-            poke_data = {
-                "id": poke_id,
-                "name": poke_name,
-                "lvl": poke_level,
-                "avgIV": poke_avgIV,
-            }
-            return poke_data
-        else:
-            return 0
+                poke_data = {
+                    "id": poke_id,
+                    "name": poke_name,
+                    "lvl": poke_level,
+                    "avgIV": poke_avgIV,
+                }
+                return poke_data
+            else:
+                return 0
 
     def buy_item(self, item, amount=1, refresh=True):
         data = {"amount": amount, "item_name": item}
@@ -165,47 +196,57 @@ class PokeCommu:
         if response.status_code == 200:
             print(f"Bought {amount} {item}")
             if refresh:
-                self.load_inventory()
+                self.load_inventory_api()
             return True
         return False
 
     def is_pokemon_in_inventory(self, pokemon: Pokemon) -> bool:
-        poke_name = pokemon.get_pcg_name()
+        with self.pekemon_lock:
+            poke_name = pokemon.get_pcg_name()
 
-        if any(
-            poke["name"] == poke_name
-            for poke in chain(self.pokemons, self.pokemons_shiny, self.pokemons_locked)
-        ):
-            return True
-        return False
+            if poke_name in self.exeptions_pokemons:
+                return False
+
+            if any(
+                poke["name"] == poke_name
+                for poke in chain(
+                    self.pokemons, self.pokemons_shiny, self.pokemons_locked
+                )
+            ):
+                return True
+            return False
 
     def is_pokemon_in_pokedex(self, pokemon: Pokemon) -> bool:
-        poke_name = pokemon.get_pcg_name()
-        return self.pokedex.get(poke_name, False)
+        with self.pokedex_lock:
+            poke_name = pokemon.get_pcg_name()
+            return self.pokedex.get(poke_name, False)
 
     def is_shiny_in_inventory(self, pokemon: Pokemon) -> bool:
-        poke_name = pokemon.get_pcg_name()
+        with self.pekemon_lock:
+            poke_name = pokemon.get_pcg_name()
 
-        if any(poke["name"] == poke_name for poke in self.pokemons_shiny):
-            return True
-        return False
+            if any(poke["name"] == poke_name for poke in self.pokemons_shiny):
+                return True
+            return False
 
     def check_ball_in_inventary(self, ball) -> bool:
-        if [b for b in self.inventory if b["name"] == ball]:
-            ball = [b for b in self.inventory if b["name"] == ball][0]
-            if ball["amount"] > 0:
-                return True
+        with self.inventory_lock:
+            if [b for b in self.inventory if b["name"] == ball]:
+                ball = [b for b in self.inventory if b["name"] == ball][0]
+                if ball["amount"] > 0:
+                    return True
 
-        return False
+            return False
 
     def remove_ball_from_inventory(self, ball) -> bool:
-        if [b for b in self.inventory if b["name"] == ball]:
-            ball = [b for b in self.inventory if b["name"] == ball][0]
-            if ball["amount"] > 0:
-                ball["amount"] -= 1
-                return True
+        with self.inventory_lock:
+            if [b for b in self.inventory if b["name"] == ball]:
+                ball = [b for b in self.inventory if b["name"] == ball][0]
+                if ball["amount"] > 0:
+                    ball["amount"] -= 1
+                    return True
 
-        return False
+            return False
 
     def __auto_buy_ultraball(self):
         if [b for b in self.inventory if b["name"] == "ultra_ball"]:
@@ -220,8 +261,17 @@ class PokeCommu:
 
 if __name__ == "__main__":
     pokeCommu = PokeCommu()
-    for pokemon in pokeCommu.pokemons:
-        # if "unown" in pokemon["name"]:
-        #     print(pokemon)
-        if "mime" in pokemon["name"]:
-            print(pokemon)
+    pikachu = Pokemon(name_en="pikachu")
+    vulpix_aloa = Pokemon(name_en="vulpix", reg_form="alola")
+    vulpix = Pokemon(name_en="vulpix")
+
+    print(pokeCommu.is_pokemon_in_pokedex(pikachu))
+    print(pokeCommu.is_pokemon_in_pokedex(vulpix_aloa))
+    print(pokeCommu.is_pokemon_in_pokedex(vulpix))
+
+    # pokeCommu.load_inventory()
+    # for pokemon in pokeCommu.pokemons:
+    #     # if "unown" in pokemon["name"]:
+    #     #     print(pokemon)
+    #     if "mime" in pokemon["name"]:
+    #         print(pokemon)
